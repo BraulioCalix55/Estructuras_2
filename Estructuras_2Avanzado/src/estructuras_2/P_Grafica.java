936b6c01599b1f2895a3b53e313ca0b5f543766c
BraulioCalix55 <djbrzhn1@hotmail.com>
2019-11-13 01:53:05 -0600

    correcion meta

--- Estructuras_2Avanzado/src/estructuras_2/P_Grafica.java
+++ Estructuras_2Avanzado/src/estructuras_2/P_Grafica.java
@@ -357,11 +357,11 @@ public class P_Grafica extends javax.swing.JFrame {
         JOptionPane.showMessageDialog(Nuevo_campos, "Campo agregado con exito ");
         Tx_campo.setText("Nombre del Campo");
         temp.add(new Campos(campo, tipo));
-        String Mensaje=""+temp.size()+";";
+        String Mensaje=""+temp.size()+";0;1;";
         for (int i = 0; i < temp.size(); i++) {
             Mensaje+=temp.get(i).toString();
         }
-        Mensaje+=";0;-1;";
+        Mensaje+=";";
         try {
             FileWriter fw = new FileWriter(archivo);
             BufferedWriter bw = new BufferedWriter(fw);
