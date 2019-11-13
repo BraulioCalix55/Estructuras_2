ea328bcb9aa5e47d045384d858913237b72970aa
RickyRom <30517351+RickyRom@users.noreply.github.com>
2019-11-12 23:47:11 -0600

    avance

--- Estructuras_2Avanzado/src/estructuras_2/leer_archivo.java
+++ Estructuras_2Avanzado/src/estructuras_2/leer_archivo.java
@@ -6,6 +6,7 @@
 package estructuras_2;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -13,17 +14,40 @@ import java.util.Scanner;
  * @author User
  */
 public class leer_archivo {
-    public static void main(String ruta) {
+    public static Metadata main(String ruta) {
+        ArrayList <Campos>lista_campos= new ArrayList();
+        Metadata metadata= null;
         try {
             Scanner input = new Scanner(new File(ruta));
             while (input.hasNextLine()) {
-                String line = input.nextLine();
-                System.out.println("plop");
-                System.out.println(line);
+                String line = input.next();
+                String []arreglo=line.split(";");
+                int num_campos=Integer.parseInt(arreglo[0]);
+                boolean registros;
+                if (arreglo[1]=="0") {
+                    registros=false;
+                }else{
+                    registros=true;
+                }
+                int cabeza=Integer.parseInt(arreglo[2]);
+                String campos[]=arreglo[3].split(",");
+                ArrayList lista= new ArrayList();
+                for (int i = 0; i < campos.length-1; i++) {
+                    String nombre[]=campos[i].split("=");
+                    lista.add(nombre[1]);
+                    i++;
+                }
+                System.out.println("num campos: "+(num_campos)*2);
+                for (int i = 0; i < (lista.size())/2; i++) {
+                    lista_campos.add(new Campos(lista.get(i).toString(), lista.get(i+1).toString()));
+                }
+                metadata= new Metadata(num_campos, lista_campos, registros, cabeza);
+                
             }
             input.close();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+        return metadata;
     }
 }
