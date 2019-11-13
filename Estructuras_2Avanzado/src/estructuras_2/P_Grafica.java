296808c72b9095d94faf24ba24bdf921c7fb305c
RickyRom <30517351+RickyRom@users.noreply.github.com>
2019-11-13 00:09:46 -0600

    cambio visual

--- Estructuras_2Avanzado/src/estructuras_2/P_Grafica.java
+++ Estructuras_2Avanzado/src/estructuras_2/P_Grafica.java
@@ -429,7 +429,7 @@ public class P_Grafica extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
--- Estructuras_2Avanzado/src/estructuras_2/leer_archivo.java
+++ Estructuras_2Avanzado/src/estructuras_2/leer_archivo.java
@@ -6,6 +6,7 @@
 package estructuras_2;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -14,48 +15,47 @@ import java.util.Scanner;
  * @author User
  */
 public class leer_archivo {
-    public static Metadata main(String ruta) {
-        ArrayList <Campos>lista_campos= new ArrayList();
-        Metadata metadata= null;
+
+    public static Metadata main(String ruta) throws FileNotFoundException {
+        ArrayList<Campos> lista_campos = new ArrayList();
+        Metadata metadata = null;
         try {
             Scanner input = new Scanner(new File(ruta));
             while (input.hasNextLine()) {
                 String line = input.next();
-                String []arreglo=line.split(";");
-                int num_campos=Integer.parseInt(arreglo[0]);
+                String[] arreglo = line.split(";");
+                int num_campos = Integer.parseInt(arreglo[0]);
                 boolean registros;
-                if (arreglo[1]=="0") {
-                    registros=false;
-                }else{
-                    registros=true;
+                if (arreglo[1] == "0") {
+                    registros = false;
+                } else {
+                    registros = true;
                 }
-                int cabeza=Integer.parseInt(arreglo[2]);
-                String campos[]=arreglo[3].split(",");
-                ArrayList lista= new ArrayList();
-<<<<<<< HEAD
-                for (int i = 0; i < campos.length-1; i++) {
-                    String nombre[]=campos[i].split("=");
+                int cabeza = Integer.parseInt(arreglo[2]);
+                String campos[] = arreglo[3].split(",");
+                ArrayList lista = new ArrayList();
+                for (int i = 0; i < campos.length - 1; i++) {
+                    String nombre[] = campos[i].split("=");
                     lista.add(nombre[1]);
                     i++;
                 }
-                System.out.println("num campos: "+(num_campos)*2);
-                for (int i = 0; i < (lista.size())/2; i++) {
-                    lista_campos.add(new Campos(lista.get(i).toString(), lista.get(i+1).toString()));
-=======
-                for (int i = 0; i < campos.length; i++) {
-                    String nombre[]=campos[i].split("=");
-                    lista.add(nombre[1]);
+                System.out.println("num campos: " + (num_campos) * 2);
+                for (int j = 0; j < (lista.size()) / 2; j++) {
+                    lista_campos.add(new Campos(lista.get(j).toString(), lista.get(j + 1).toString()));
+                    for (int i = 0; i < campos.length; i++) {
+                        String nombre[] = campos[i].split("=");
+                        lista.add(nombre[1]);
+                    }
+
+                    for (int i = 0; i < lista.size(); i++) {
+                        lista_campos.add(new Campos(lista.get(i).toString(), lista.get(i + 1).toString()));
+                        i++;
+                    }
+                    metadata = new Metadata(num_campos, lista_campos, registros, cabeza);
+
                 }
-                
-                for (int i = 0; i < lista.size(); i++) {
-                    lista_campos.add(new Campos(lista.get(i).toString(), lista.get(i+1).toString()));
-                    i++;
->>>>>>> edb2366bbb8cb74bd65fcb82681272af973280fc
-                }
-                metadata= new Metadata(num_campos, lista_campos, registros, cabeza);
-                
+                input.close();
             }
-            input.close();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
