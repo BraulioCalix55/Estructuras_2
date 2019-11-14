bcaeb8ce32054031519e3f9d24a37d82e630135e
BraulioCalix55 <djbrzhn1@hotmail.com>
2019-11-14 02:58:39 -0600

    reponer

---
+++ Estructuras_2Avanzado/src/estructuras_2/Campos.java
@@ -0,0 +1,41 @@
+
+package estructuras_2;
+import java.io.Serializable;
+/**
+ *
+ * @author User
+ */
+public class Campos implements Serializable{
+     private String nombre;
+    private String tipo;
+
+    public Campos() {
+    }
+
+    public Campos(String nombre, String tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "nombre=" + nombre + ",tipo=" + tipo+",";
+    }
+    
+}
---
+++ Estructuras_2Avanzado/src/estructuras_2/leer_archivo.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructuras_2;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class leer_archivo {
+    public static Metadata main(String ruta) {
+        ArrayList <Campos>lista_campos= new ArrayList();
+        Metadata metadata= null;
+        try {
+            Scanner input = new Scanner(new File(ruta));
+            while (input.hasNextLine()) {
+                String line = input.next();
+                String []arreglo=line.split(";");
+                int num_campos=Integer.parseInt(arreglo[0]);
+                boolean registros;
+                if (arreglo[1]=="0") {
+                    registros=false;
+                }else{
+                    registros=true;
+                }
+                int cabeza=Integer.parseInt(arreglo[2]);
+                String campos[]=arreglo[3].split(",");
+                ArrayList lista= new ArrayList();
+                for (int i = 0; i < campos.length; i++) {
+                    String nombre[]=campos[i].split("=");
+                    lista.add(nombre[1]);
+                }
+                for (int i = 0; i < lista.size(); i++) {
+                    lista_campos.add(new Campos(lista.get(i).toString(), lista.get(i+1).toString()));
+                    i++;
+                }
+                metadata= new Metadata(num_campos, lista_campos, registros, cabeza);
+            }
+            input.close();
+        } catch (Exception ex) {
+            //ex.printStackTrace();
+        }
+        return metadata;
+    }
+}
